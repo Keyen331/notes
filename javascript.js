965dd236200fb0d93cf5ace9ee6976cadfc85f44
Keyen331 <banayos.kay@gmail.com>
2016-10-17 08:47:25 -1000

    updating

---
+++ javascript.js
@@ -0,0 +1,39 @@
+//This is a comment
+/* this is a block comment */
+
+// DATA TYPES
+
+// this is string
+var myName = "Kay";
+
+// this is a integer
+var myAge = 16;
+
+// boolen type
+var booleanexample = true;
+
+// unassigned variable
+undefined
+
+// not a number
+NaN
+
+// array
+
+// objects
+
+// DATA TYPES
+
+var camelCase
+
+// = VS ==
+// = is for assignment
+// == is for comparing
+
+/* Boolean operators
+// ==
+// !== not equal
+//
+
+Logical Operator
+// && : or
